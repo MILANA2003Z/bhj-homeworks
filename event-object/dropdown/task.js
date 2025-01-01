7a8694a07eeec58759e9d9e56190f8a5616ef741
MILANA2003Z <96334845+MILANA2003Z@users.noreply.github.com>
2025-01-01 21:22:31 +0300

    Update task.js

---
+++ event-object/dropdown/task.js
@@ -0,0 +1,30 @@
+const dropdowns = document.querySelectorAll('.dropdown');
+
+    dropdowns.forEach(dropdown => {
+        const value = dropdown.querySelector('.dropdown__value');
+        const list = dropdown.querySelector('.dropdown__list');
+        const items = dropdown.querySelectorAll('.dropdown__item');
+
+        // Открытие/закрытие списка по клику на выбранное значение
+        value.addEventListener('click', () => {
+            list.classList.toggle('dropdown__list_active');
+        });
+
+        // Установка нового значения при выборе пункта меню
+        items.forEach(item => {
+            item.addEventListener('click', (event) => {
+                event.preventDefault();  // Запрещаем переход по ссылке
+                const selectedText = item.textContent.trim();
+                value.textContent = selectedText;
+                list.classList.remove('dropdown__list_active');
+            });
+        });
+
+        // Закрытие списка при клике вне выпадающего меню
+        document.addEventListener('click', (event) => {
+            if (!dropdown.contains(event.target)) {
+                list.classList.remove('dropdown__list_active');
+            }
+        });
+    });
+});
